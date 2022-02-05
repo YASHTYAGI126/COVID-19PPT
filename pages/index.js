2e770a021ef5c04a6eb1246d87470408369999d0
YASHTYAGI126 <98898616+YASHTYAGI126@users.noreply.github.com>
2022-02-05 13:59:02 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <a href="https://download1075.mediafire.com/sle3ciz6kwgg/aarkruqzb4ls3d7/covid+ppt+.pptx"> click here for get covid 19 ppt </a>
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
